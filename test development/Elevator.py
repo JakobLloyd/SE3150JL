94350c4cc301a7b546306b4a29e43843ef3dfdd9
Jakob <jkblloyd@gmail.com>
2026-09-10 15:23:50 -0600

    green: validate elevator destination floors

--- test development/Elevator.py
+++ test development/Elevator.py
@@ -17,6 +17,8 @@ class Elevator:
         }
 
     def request(self, floor):
+        if floor not in {1, 2, 3}:
+            raise ValueError("destination must be 1, 2, or 3")
         if floor != self.floor and floor not in self.destinations:
             self.destinations.append(floor)
 
