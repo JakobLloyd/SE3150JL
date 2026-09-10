4d76e51a402c48133b1a5b8b1bb1b1f264486d1d
Jakob <jkblloyd@gmail.com>
2026-09-10 15:07:44 -0600

    red: specify elevator idle state

--- test development/test_elevator.py
+++ test development/test_elevator.py
@@ -6,3 +6,6 @@ from Elevator import Elevator
 def describe_elevator():
     def it_starts_on_floor_one():
         assert Elevator().floor == 1
+
+    def it_starts_idle():
+        assert Elevator().direction == "Idle"
