ae57331abacf5c3a6ef2a73a660a217d6e86a43b
Jakob <jkblloyd@gmail.com>
2026-09-10 15:30:19 -0600

    green: stop elevator movement in emergency

--- test development/Elevator.py
+++ test development/Elevator.py
@@ -29,7 +29,7 @@ class Elevator:
         self.destinations.append(floor)
 
     def step(self):
-        if self.passengers > self.capacity or not self.destinations:
+        if self.emergency or self.passengers > self.capacity or not self.destinations:
             return
         if self.doors == "Open":
             self.close_doors()
