f8ec0cce9cbc1ecd0df4e61cd22d9b0a207f785e
Jakob <jkblloyd@gmail.com>
2026-09-24 15:17:41 -0600

    Added squirrel handlers

--- doubles/test_squirrel_server.py
+++ doubles/test_squirrel_server.py
@@ -1,1 +1,31 @@
-import pytest
+import json
+
+from squirrel_server import SquirrelServerHandler
+
+
+def describe_SquirrelServerHandler():
+    def describe_handleSquirrelsIndex():
+        def it_returns_the_squirrel_collection(mocker):
+            request = SquirrelServerHandler.__new__(SquirrelServerHandler)
+            request.send_response = mocker.Mock()
+            request.send_header = mocker.Mock()
+            request.end_headers = mocker.Mock()
+            request.wfile = mocker.Mock()
+            mock_db = mocker.patch("squirrel_server.SquirrelDB")
+            squirrels = [
+                {"id": 1, "name": "kyanne", "size": "small"},
+                {"id": 2, "name": "bingus", "size": "large"},
+            ]
+            mock_db.return_value.getSquirrels.return_value = squirrels
+
+            request.handleSquirrelsIndex()
+
+            mock_db.return_value.getSquirrels.assert_called_once_with()
+            request.send_response.assert_called_once_with(200)
+            request.send_header.assert_called_once_with(
+                "Content-Type", "application/json"
+            )
+            request.end_headers.assert_called_once_with()
+            request.wfile.write.assert_called_once_with(
+                bytes(json.dumps(squirrels), "utf-8")
+            )
