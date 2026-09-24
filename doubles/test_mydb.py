aac4d0e9cd532d0f1b9f6b983aacc4c8d035c287
Jakob <jkblloyd@gmail.com>
2026-09-24 14:13:21 -0600

    Test MyDB missing database initialization

--- doubles/test_mydb.py
+++ doubles/test_mydb.py
@@ -12,12 +12,10 @@ def describe_MyDB():
             assert db.fname == "test.db"
 
 def it_creates_empty_database_if_it_does_not_exist(mocker):
-    mock_isfile = mocker.patch("os.path.isfile", return_value=False)
-    mock_open = mocker.patch("builtins.open", mocker.mock_open())
-    mock_dump = mocker.patch("pickle.dump")
-    db = MyDB("test.db")
+    mock_isfile = mocker.patch("mydb.os.path.isfile", return_value=False)
+    mock_save_strings = mocker.patch.object(MyDB, "saveStrings")
 
-    mock_isfile.assert_called_once_with("somefilename")
-    mock_open.assert_called_once_with("somefilename", 'wb')
-    mock_dump.assert_called_once_with([], mock_open())
-    assert os.path.isfile("somefilename") == False
+    MyDB("test.db")
+
+    mock_isfile.assert_called_once_with("test.db")
+    mock_save_strings.assert_called_once_with([])
