53e0bdbb3605cf07a3fda54994450ef92ab45af4
Jakob <jkblloyd@gmail.com>
2026-09-24 14:21:59 -0600

    Add MyDB loadStrings test

--- doubles/test_mydb.py
+++ doubles/test_mydb.py
@@ -11,6 +11,19 @@ def describe_MyDB():
             db = MyDB("test.db")
             assert db.fname == "test.db"
 
+    def describe_loadStrings():
+        def it_loads_strings_from_the_database(mocker):
+            mocker.patch("mydb.os.path.isfile", return_value=True)
+            mock_open = mocker.patch("mydb.open", mocker.mock_open())
+            mock_load = mocker.patch("mydb.pickle.load", return_value=["one", "two"])
+            db = MyDB("test.db")
+
+            result = db.loadStrings()
+
+            assert result == ["one", "two"]
+            mock_open.assert_called_once_with("test.db", "rb")
+            mock_load.assert_called_once_with(mock_open.return_value.__enter__.return_value)
+
 def it_creates_empty_database_if_it_does_not_exist(mocker):
     mock_isfile = mocker.patch("mydb.os.path.isfile", return_value=False)
     mock_save_strings = mocker.patch.object(MyDB, "saveStrings")
