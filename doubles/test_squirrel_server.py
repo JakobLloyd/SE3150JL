c519e6322cf9e54bb0efb86ea852b8dce9b37a53
Jakob <jkblloyd@gmail.com>
2026-09-24 15:23:12 -0600

    Test squirrel retrieval

--- doubles/test_squirrel_server.py
+++ doubles/test_squirrel_server.py
@@ -29,3 +29,26 @@ def describe_SquirrelServerHandler():
             request.wfile.write.assert_called_once_with(
                 bytes(json.dumps(squirrels), "utf-8")
             )
+
+    def describe_handleSquirrelsRetrieve():
+        def it_returns_a_squirrel_when_it_exists(mocker):
+            request = SquirrelServerHandler.__new__(SquirrelServerHandler)
+            request.send_response = mocker.Mock()
+            request.send_header = mocker.Mock()
+            request.end_headers = mocker.Mock()
+            request.wfile = mocker.Mock()
+            mock_db = mocker.patch("squirrel_server.SquirrelDB")
+            squirrel = {"id": 1, "name": "kyanne", "size": "small"}
+            mock_db.return_value.getSquirrel.return_value = squirrel
+
+            request.handleSquirrelsRetrieve("1")
+
+            mock_db.return_value.getSquirrel.assert_called_once_with("1")
+            request.send_response.assert_called_once_with(200)
+            request.send_header.assert_called_once_with(
+                "Content-Type", "application/json"
+            )
+            request.end_headers.assert_called_once_with()
+            request.wfile.write.assert_called_once_with(
+                bytes(json.dumps(squirrel), "utf-8")
+            )
