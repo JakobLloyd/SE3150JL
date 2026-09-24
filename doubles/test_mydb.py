3c30867b1043354a01b7a828daa81c5d0555cff4
Jakob <jkblloyd@gmail.com>
2026-09-24 14:24:31 -0600

    Add MyDB saveStrings test

--- doubles/test_mydb.py
+++ doubles/test_mydb.py
@@ -24,6 +24,20 @@ def describe_MyDB():
             mock_open.assert_called_once_with("test.db", "rb")
             mock_load.assert_called_once_with(mock_open.return_value.__enter__.return_value)
 
+    def describe_saveStrings():
+        def it_saves_strings_to_the_database(mocker):
+            mocker.patch("mydb.os.path.isfile", return_value=True)
+            mock_open = mocker.patch("mydb.open", mocker.mock_open())
+            mock_dump = mocker.patch("mydb.pickle.dump")
+            db = MyDB("test.db")
+
+            db.saveStrings(["one", "two"])
+
+            mock_open.assert_called_once_with("test.db", "wb")
+            mock_dump.assert_called_once_with(
+                ["one", "two"], mock_open.return_value.__enter__.return_value
+            )
+
 def it_creates_empty_database_if_it_does_not_exist(mocker):
     mock_isfile = mocker.patch("mydb.os.path.isfile", return_value=False)
     mock_save_strings = mocker.patch.object(MyDB, "saveStrings")
